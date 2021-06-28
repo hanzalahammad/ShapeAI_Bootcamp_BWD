88abd4df1657585148ee2181d149c01a848b4f33
hanzalahammad <76391350+hanzalahammad@users.noreply.github.com>
2021-06-28 17:38:17 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by ShapeAI @ {new Date().getFullyear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
